5d5d5a2716aa6d80bbc2c035035452b8435c612b
Adam Thornton <athornton@lsst.org>
2024-07-09 13:48:57 -0700

    Rationalize startup files

--- profile.d/local06-setupuser.sh
+++ profile.d/local06-setupuser.sh
@@ -6,20 +6,22 @@
 
 # In a production environment, there will be a lot more you want to do.
 
+# Create user data structures
+
 for d in WORK DATA notebooks; do
-    if [ ! -d "${HOME}/${d}" ]; then
-        mkdir -p "${HOME}/${d}"
-    fi
+    mkdir -p "${HOME}/${d}"
 done
 
-my_dircolors="${HOME}/.dir_colors"
+# Copy anything out of /etc/skel if it's not in the user dir already
+cp -an /etc/skel/* /etc/skel/.* "${HOME}" 2>/dev/null
+
 # Copy dircolors if needed
+my_dircolors="${HOME}/.dir_colors"
 if [ ! -f "${my_dircolors}" ]; then
     cp /etc/dircolors.ansi-universal "${my_dircolors}"
 fi
 
 # Activate dircolors
-eval $(dircolors -b "{my_dircolors}")
+eval $(dircolors -b "${my_dircolors}")
 
 alias ls='ls --color=auto'
-    
--- runtime/spherexlaunch.bash
+++ runtime/spherexlaunch.bash
@@ -2,7 +2,7 @@
 CONFIG_FILE=$1
 
 # Load the appropriate conda environment
-source /opt/spherex/runtime/loadspherex
+. /opt/spherex/runtime/loadspherex
 
 # And now transfer control over to Python
 exec python3 -m ipykernel -f ${CONFIG_FILE}
